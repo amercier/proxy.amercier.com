1a9e4754c9256811f44b9157e0634e5e25958d57
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-03-15 22:26:32 +0100

    Added more checks

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,18 @@
 
-$url = window.location.href.replace(/test\/(index\.html)?$/, '');
+url = window.location.href.replace(/test\/(index\.html)?(\?.*)?(#.*)?$/, '');
 
 asyncTest('Allowed domain', function() {
-  $.ajax($url + 'http://www.google.com', {
+  $.ajax(url + url + 'test/', { // Oh my, how meta
     headers: {
       'Accept': 'text/html'
     }
   })
     .done(function(response, status, deferred) {
       ok(true, '[' + deferred.status + '] ' + deferred.statusText + ' - ' + deferred.responseText);
+      strictEqual(deferred.status, 200, 'Status code should be 200');
+      strictEqual(deferred.statusText, 'OK', 'Status text should be "OK"');
+      ok(/^<!DOCTYPE /.test(response), 'Response should start with "<!DOCTYPE "');
+      ok(/<title>proxy.amercier.com<\/title>/.test(response), 'Response should contain "<title>proxy.amercier.com</title>"');
       start();
     })
     .fail(function(deferred, status, response) {
@@ -17,8 +21,8 @@ asyncTest('Allowed domain', function() {
     });
 });
 
-asyncTest('Forbidden domain', function() {
-  $.ajax($url + 'http://www.google.cn', {
+asyncTest('Not allowed domain', function() {
+  $.ajax(url + 'http://www.iana.org/domains/example', {
     headers: {
       'Accept': 'text/html'
     }
@@ -32,3 +36,4 @@ asyncTest('Forbidden domain', function() {
       start();
     });
 });
+
